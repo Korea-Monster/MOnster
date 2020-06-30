0bbd65a4ca3308fe455438d3d32783469435e9e7
Korea-Monster <67091182+Korea-Monster@users.noreply.github.com>
2020-07-01 02:43:15 +0900

    Update app.js

--- app.js
+++ app.js
@@ -22,15 +22,103 @@ burger.addEventListener('click',() => {
 
 navSide();
 
-
-
 //CONTENT
 function fnTest1(divName) {
   var frm = document.getElementById(divName);
-
+  document.getElementById("iframe_s").src = "./동물/animal.html"
   if (frm.style.display != "block") {
    frm.style.display = "block";
  } else{
    frm.style.display = "none";
   }
  }
+
+ function fnTest2(divName) {
+   var frm = document.getElementById(divName);
+   document.getElementById("iframe_s").src = "./동물/animal2.html"
+   if (frm.style.display != "block") {
+    frm.style.display = "block";
+  } else{
+    frm.style.display = "none";
+   }
+  }
+
+  function fnTest3(divName) {
+    var frm = document.getElementById(divName);
+    document.getElementById("iframe_s").src = "./동물/animal3.html"
+    if (frm.style.display != "block") {
+     frm.style.display = "block";
+   } else{
+     frm.style.display = "none";
+    }
+   }
+
+   function fnTest4(divName) {
+     var frm = document.getElementById(divName);
+     document.getElementById("iframe_s").src = "./동물/animal4.html"
+     if (frm.style.display != "block") {
+      frm.style.display = "block";
+    } else{
+      frm.style.display = "none";
+     }
+    }
+
+    function fnTest5(divName) {
+      var frm = document.getElementById(divName);
+      document.getElementById("iframe_s").src = "./동물/animal5.html"
+      if (frm.style.display != "block") {
+       frm.style.display = "block";
+     } else{
+       frm.style.display = "none";
+      }
+     }
+
+     function fnTest6(divName) {
+       var frm = document.getElementById(divName);
+       document.getElementById("iframe_s").src = "./동물/animal6.html"
+       if (frm.style.display != "block") {
+        frm.style.display = "block";
+      } else{
+        frm.style.display = "none";
+       }
+      }
+
+      function fnTest7(divName) {
+        var frm = document.getElementById(divName);
+        document.getElementById("iframe_s").src = "./동물/animal7.html"
+        if (frm.style.display != "block") {
+         frm.style.display = "block";
+       } else{
+         frm.style.display = "none";
+        }
+       }
+
+       function fnTest8(divName) {
+         var frm = document.getElementById(divName);
+         document.getElementById("iframe_s").src = "./동물/animal8.html"
+         if (frm.style.display != "block") {
+          frm.style.display = "block";
+        } else{
+          frm.style.display = "none";
+         }
+        }
+
+        function fnTest9(divName) {
+          var frm = document.getElementById(divName);
+          document.getElementById("iframe_s").src = "./동물/animal9.html"
+          if (frm.style.display != "block") {
+           frm.style.display = "block";
+         } else{
+           frm.style.display = "none";
+          }
+         }
+
+         function fnTest10(divName) {
+           var frm = document.getElementById(divName);
+           document.getElementById("iframe_s").src = "./동물/animal10.html"
+           if (frm.style.display != "block") {
+            frm.style.display = "block";
+          } else{
+            frm.style.display = "none";
+           }
+          }
